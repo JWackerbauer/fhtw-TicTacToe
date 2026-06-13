2d6b9e928cdf1baa7dc25dadae18af41b0234e16
Berzan Gül <cberzangul@icloud.com>
2026-06-13 23:44:03 +0200

    US-3 Spielende erkennen implementiert

--- tictactoe/src/main/java/at/fhtw/tictactoe/TicTacToe.java
+++ tictactoe/src/main/java/at/fhtw/tictactoe/TicTacToe.java
@@ -1,5 +1,5 @@
 package at.fhtw.tictactoe;
-
+import java.util.Scanner;
 public class TicTacToe {
     // Private Attribute laut Diagramm
     private Player player1;
@@ -17,17 +17,34 @@ public class TicTacToe {
 
     // Startet die Spielsteuerung
     public void start() {
-        int row = 0;
-        int col = 0;
+        Scanner scanner = new Scanner(System.in);
+        boolean gameOver = false;
 
-        // Prüfen, ob das Feld frei ist und Symbol platzieren (US1)
-        if (board.isCellEmpty(row, col)) {
-            board.place(row, col, currentPlayer.getMarker());
+        while (!gameOver) {
+            System.out.println("Current Player: " + currentPlayer.getMarker());
             board.display();
-            System.out.println("Spieler " + currentPlayer.getMarker() + " hat auf Feld (" + row + "," + col + ") gesetzt.");
-        }
+            System.out.print("row (0-2): ");
+            int row = scanner.nextInt();
+            System.out.print("column (0-2): ");
+            int col = scanner.nextInt();
 
-        switchCurrentPlayer();
+            if (board.isCellEmpty(row, col)) {
+                board.place(row, col, currentPlayer.getMarker());
+                board.display();
+
+                if (hasWinner()) {
+                    System.out.println("Spieler " + currentPlayer.getMarker() + " hat gewonnen!");
+                    gameOver = true;
+                } else if (board.isFull()) {
+                    System.out.println("Unentschieden!");
+                    gameOver = true;
+                } else {
+                    switchCurrentPlayer();
+                }
+            } else {
+                System.out.println("Feld ist schon besetzt! Nochmal versuchen.");
+            }
+        }
     }
 
     // Wechselt den aktuellen Spieler (private)
@@ -41,6 +58,32 @@ public class TicTacToe {
 
     // Prüft, ob es einen Gewinner gibt (private)
     private boolean hasWinner() {
+        // Zeilen prüfen
+        for (int i = 0; i < 3; i++) {
+            if (board.getCell(i, 0) != ' ' &&
+                    board.getCell(i, 0) == board.getCell(i, 1) &&
+                    board.getCell(i, 1) == board.getCell(i, 2)) {
+                return true;
+            }
+        }
+        // Spalten prüfen
+        for (int i = 0; i < 3; i++) {
+            if (board.getCell(0, i) != ' ' &&
+                    board.getCell(0, i) == board.getCell(1, i) &&
+                    board.getCell(1, i) == board.getCell(2, i)) {
+                return true;
+            }
+        }
+        // Diagonalen prüfen
+        if (board.getCell(0, 0) != ' ' &&
+                board.getCell(0, 0) == board.getCell(1, 1) &&
+                board.getCell(1, 1) == board.getCell(2, 2)) {
+            return true;
+        }
+        if (board.getCell(0, 2) != ' ' &&
+                board.getCell(0, 2) == board.getCell(1, 1) &&
+                board.getCell(1, 1) == board.getCell(2, 0)) {
+            return true;
+        }
         return false;
-    }
-}
+    }}
